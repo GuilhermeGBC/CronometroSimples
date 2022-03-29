35445efdbc77e6e0afb1bb4cb499ce256de1a1b3
Guilherme Bastos <gbcguilherme@hotmail.com>
2022-03-29 13:06:17 -0300

    Adicionar arquivos de projeto.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PreStart(100);
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string dados = Console.ReadLine().ToLower();
+            char type = char.Parse(dados.Substring(dados.Length - 1, 1));
+            int time = int.Parse(dados.Substring(0, dados.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            PreStart(time + multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.WriteLine("Preparar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Apontar...");           
+            Thread.Sleep(1000);
+            Console.WriteLine("VAI!");
+            Thread.Sleep(2500);
+            Start(time);
+
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado... Retornando para o menu");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
